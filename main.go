401b7c5586a4e1fb589b40f26f8621bc8319c857
Ishxan Nikolyan <ishxannikolyan7@gmail.com>
2026-03-22 16:16:31 +0400

    sarqinq proekty

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
-import "fmt"
+import "golang-lessons/services"
 
 func main() {
-	printint(12)
-	printstring("dfjhbud")
-	sum, sub, div, _ := calc1(15, 75)
-	fmt.Println(sum, sub, div)
+	services.Print()
 }
